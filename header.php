8a59480570a91fc3bc4c3e4c35e1ef302733f082
hurtix <hurtix@gmail.com>
2025-08-12 14:16:41 -0500

    Add Basecoat UI lib

--- header.php
+++ header.php
@@ -7,9 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Imperium - Café Cauca</title>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basecoat-css@0.3.1/dist/basecoat.cdn.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/basecoat-css@0.3.1/dist/js/all.min.js" defer></script>
-  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="/assets/libs/basecoat/basecoat.cdn.min.css">
+  <script src="/assets/libs/basecoat/basecoat.cdn.min.js" defer></script>
+  <link rel="stylesheet" href="/assets/css/styles.css">
 
 </head>
 
